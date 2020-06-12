8b9835a80c65f9b3b3cbecebabebcc248b893931
GitTanmai <58459132+GitTanmai@users.noreply.github.com>
2020-06-12 10:37:10 +0530

    New uploaded code of product detail

--- amzproduct_detail.py
+++ amzproduct_detail.py
@@ -12,7 +12,7 @@ import numpy as np
 
 options = Options()
 di={}
-column_names=['Product_Id','Product_Name','Price','Onshelfavalaiblity','Returnable','Sold_by','Rating','Delivered_by','Best_Before']
+column_names=['Product_Id','Product_Name','Product url','MRP','Price','Discount','Onshelfavalaiblity','Returnable','Sold_by','Rating','Total Rating count','Delivered_by','Best_Before']
 df = pd.DataFrame(columns = column_names)
 #print('cols',df.columns)
 
@@ -33,11 +33,25 @@ def details(links,df):
                 pro = driver.find_element_by_xpath("//*[@id='productTitle']")
                 di['Product_Name']=pro.text
 
+                #add url
+                di['Product url']=li
                 #Adding price to dict
+                try:
+                        orprice=driver.find_element_by_class_name("priceBlockStrikePriceString")
+                        di['MRP']=orprice.text[2:]
+                except:
+                        price_el=driver.find_element_by_xpath("//*[@id='priceblock_ourprice']")
+                        di['MRP']=price_el.text[2:]
 
                 price_el=driver.find_element_by_xpath("//*[@id='priceblock_ourprice']")
-                di['Price']=price_el.text
+                di['Price']=price_el.text[2:]
 
+                #Discount in price
+                try:
+                        dis=driver.find_element_by_xpath("//*[@id='regularprice_savings']/td[2]")
+                        di['Discount']=re.search(r'\((.*?)\)',dis.text).group(1)
+                except:
+                        di['Discount']='No Discount'
                 #Adding availability in dict
                 stock=driver.find_element_by_xpath("//*[@id='availability']/span")
                 if stock.text=='In stock.':
@@ -63,10 +77,13 @@ def details(links,df):
                 try:
                         rating=driver.find_element_by_xpath("//*[@id='merchant-info']")
                         rat=re.search(r'\((.*?)\)',rating.text).group(1)
+                        di['Rating']=float(rat[:3])
+                        di['Total Rating count']=re.search(r'\|(\s)(.[\d,]+)',rating.text).group(2).strip()
                 except:
                         rat=None
-                di['Rating']=rat
-
+                        di['Rating']='NA'
+                        di['Total Rating count']='NA'
+                #re.search(r'\|(\s)(.*[^\S])',rating.text).group(2).strip()
                 #Delivery by
                 try:
                         deliver=driver.find_element_by_xpath("//*[@id='ddmDeliveryMessage']/span")
@@ -91,7 +108,7 @@ def details(links,df):
         return df
 df=details(links,df)
 df.index = np.arange(1, len(df) + 1)
-df.to_excel("output.xlsx")
+df.to_excel("noutput.xlsx")
 print(df)
 
 
