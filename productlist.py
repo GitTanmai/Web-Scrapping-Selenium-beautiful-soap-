d1526da2100a39327f9abdd1dcf8a979ef496a78
GitTanmai <58459132+GitTanmai@users.noreply.github.com>
2020-06-23 14:58:14 +0530

    Update script
    Change in try block and adding output file

--- productlist.py
+++ productlist.py
@@ -10,20 +10,27 @@ driver = webdriver.Chrome(executable_path=r'C:\\driver\\chromedriver.exe')
 
 def plist(lis):
 	for j in lis:
-		driver.get(j)
-		soup = BeautifulSoup(driver.page_source, 'html.parser')
-		images = soup.find_all("span", class_="a-size-base-plus a-color-base a-text-normal")
-		for i in images:
-			li.append(i.text)
-		time.sleep(15)
+		try:
+			driver.get(j)
+			soup = BeautifulSoup(driver.page_source, 'html.parser')
+			images = soup.find_all("span", class_="a-size-base-plus a-color-base a-text-normal")
+			for i in images:
+				li.append(i.text)
+			time.sleep(15)
+		except:
+			print('Error occurred while fetching results')
 	print(len(li))
-	return li
+	ser=pd.Series(li)
+	return ser
 lis=['https://www.amazon.in/s?k=soap','https://www.amazon.in/s?k=soap&page=2']
-li=plist(lis)
-print(li)
+ser=plist(lis)
+ser=ser.rename('List')
+ser.index +=1
+ser.to_excel("product_list.xlsx")
 
 driver.close()
 driver.quit()
+
 
 
 #<span class="a-size-base-plus a-color-base a-text-normal" dir="auto">Dove Cream Beauty Bathing Bar, 100g (Pack of 3)</span>
