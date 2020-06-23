6cf2772d1b19abb60d5d620c22e8a9a334a67e03
GitTanmai <58459132+GitTanmai@users.noreply.github.com>
2020-06-23 13:25:12 +0530

    Update for try catch error

--- amzlogin.py
+++ amzlogin.py
@@ -9,17 +9,21 @@ options = Options()
 
 options.add_argument("window-size=1200x600")
 driver = webdriver.Chrome(executable_path=r'C:\\driver\\chromedriver.exe')
-
+user='tm81739@gmail.com'
+pwd='Tm@120894'
 driver.get("https://www.amazon.in/")
-id2=driver.find_element_by_xpath( '//*[@id="nav-signin-tooltip"]/a/span').click()
-time.sleep(10)
-
-# Opening page to enter mail id
-driver.find_element_by_xpath("//*[@id='ap_email']").send_keys('Enter email')
-time.sleep(2)
-driver.find_element_by_xpath("//*[@id='continue']").click()
-
-#Opening page to enter password
-driver.find_element_by_xpath("//*[@id='ap_password']").send_keys('Please enter password')
-time.sleep(2)
-driver.find_element_by_xpath("//*[@id='signInSubmit']").click()
+try:
+	id2=driver.find_element_by_xpath( '//*[@id="nav-signin-tooltip"]/a/span').click()
+	time.sleep(10)
+	
+	# Opening page to enter mail id
+	driver.find_element_by_xpath("//*[@id='ap_email']").send_keys(user)
+	time.sleep(2)
+	driver.find_element_by_xpath("//*[@id='continue']").click()
+	
+	#Opening page to enter password
+	driver.find_element_by_xpath("//*[@id='ap_password']").send_keys(pwd)
+	time.sleep(2)
+	driver.find_element_by_xpath("//*[@id='signInSubmit']").click()
+except:
+	print('Error occurred while signing')
